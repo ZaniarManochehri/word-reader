08727317728f7720ba6fedb80a114a6aabe045dc
Zaniar <zaniar.manochehri@gmail.com>
2024-02-07 13:47:44 +0330

    Fixed export and create pdf in App.js file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import React, { useState } from 'react';
 import mammoth from 'mammoth/mammoth.browser';
+import html2pdf from 'html2pdf.js';
 import './App.css';
 
 function App() {
   const [wordContent, setWordContent] = useState('');
 
-  // فرض کنید این تابع زمانی فراخوانی می‌شود که کاربر یک فایل ورد آپلود می‌کند
+  const handleConvertToPDF = () => {
+    const element = document.getElementById('content');
+    const opt = {
+      margin: 0.5,
+      filename: 'myfile.pdf',
+      image: { type: 'jpeg', quality: 0.70 },
+      html2canvas: { scale: 1 },
+      jsPDF: { compress: true, unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+  }
+
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
-    var path = (window.URL || window.webkitURL).createObjectURL(file);
-    console.log('Uploaded file:', file, path);
 
-    // استفاده از Mammoth برای اکسترکت کردن محتوای فایل ورد
     mammoth.extractRawText({ arrayBuffer: file })
       .then(result => {
-        console.log('Extracted text:', result.value);
-        setWordContent(result.value); // ذخیره محتوای فایل ورد در استیت
+        setWordContent(result.value);
       })
       .catch(error => {
         console.error('Error extracting text from Word file:', error);
       });
   };
 
+
   return (
-    <div>
-      <input type="file" onChange={handleFileUpload} />
-      <div>
-        <h2>محتوای فایل ورد</h2>
-        <pre>{wordContent}</pre>
+    <div className='container'>
+      <div className='content'>
+        <button onClick={handleConvertToPDF}>تبدیل به pdf</button>
+        <input type="file" onChange={handleFileUpload} />
+        <div>
+          <h2>محتوای فایل ورد</h2>
+          <pre id="content" style={{ color: 'red', fontSize: 14, textAlign: 'center' }}>{wordContent}</pre>
+        </div>
       </div>
     </div>
   );
